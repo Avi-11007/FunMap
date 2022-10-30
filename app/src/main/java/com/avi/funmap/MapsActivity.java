98b09da9681bd35aa474cf04b893ebb70a8282c1
Abhishek Pandey <tejaswinavadhitamastu777@gmail.com>
2022-10-30 15:58:34 +0530

    Initial commit

---
+++ app/src/main/java/com/avi/funmap/MapsActivity.java
@@ -0,0 +1,80 @@
+package com.avi.funmap;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.avi.funmap.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        //Additional settings in Android Maps
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng myLocation=new LatLng(26.719676861082085, 83.36320804693652);
+        mMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+
+        drawCircle(new LatLng(26.719676861082085, 83.36320804693652));
+    }
+
+    private void drawCircle(LatLng latLng){
+        CircleOptions myoptions=new CircleOptions();
+        myoptions.center(latLng);
+        myoptions.radius(120);
+        myoptions.strokeColor(Color.BLUE);
+        myoptions.strokeWidth(2);
+        myoptions.fillColor(Color.TRANSPARENT);
+        mMap.addCircle(myoptions);
+    }
+}
